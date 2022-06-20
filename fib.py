9c7915fd8e9c7818db08542d8fd2edd51471874f
István Derda <mail@istvan-derda.de>
2022-06-20 16:42:05 +0200

    init

---
+++ fib.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from functools import cache
+
+
+def main():
+    i = 1
+    while True:
+        print(fib_at(i))
+        print('\n')
+        i = i+1
+
+
+@cache
+def fib_at(position_n):
+    if position_n ==1:
+        return 1
+    if position_n == 2:
+        return 1
+    return fib_at(position_n-1) + fib_at(position_n-2)
+
+
+if __name__ == '__main__':
+    main()
